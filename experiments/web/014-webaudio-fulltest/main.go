52196ab60623f455e872bc858192dde390e2eb64
MaxKlaxx <max@tetrisk.de>
2022-07-25 16:21:22 +0200

    exp014: WAT +SquareLQ +SquareHQ

--- experiments/web/014-webaudio-fulltest/main.go
+++ experiments/web/014-webaudio-fulltest/main.go
@@ -13,12 +13,12 @@ func main() {
 	wg.Set("mainGoReady", true)
 
 	ints := make([]int32, gmconst.WorkletSampleCount)
-	fmt.Println("noise:", int32(gmsampler.Noise(ints, 123, 456)))
+	fmt.Println("noise:", int32(gmsampler.SquareHQ(ints, 123456789, 1234567890)))
 	floats := make([]float32, len(ints))
 	gmextras.ConvertIntSamplesToFloat32(floats, ints)
 
-	for i := 0; i < len(floats); i += 4 {
-		fmt.Println(i, floats[i], floats[i+1], floats[i+2], floats[i+3])
+	for i := 0; i < len(floats); i += 8 {
+		fmt.Println(i, floats[i], floats[i+1], floats[i+2], floats[i+3], floats[i+4], floats[i+5], floats[i+6], floats[i+7])
 	}
 
 	<-make(chan bool, 0)
