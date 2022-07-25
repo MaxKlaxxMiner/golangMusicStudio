7d99051ecbb87fecde1ed60415289cd593f537a2
MaxKlaxx <max@tetrisk.de>
2022-07-25 16:20:46 +0200

    exp014: +https server

--- experiments/web/000-microserver/main.go
+++ experiments/web/000-microserver/main.go
@@ -22,9 +22,18 @@ func main() {
 		panic(err)
 	}
 
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
 	http.Handle("/", http.FileServer(http.Dir(folder)))
 
 	fmt.Println("path:", folder)
+	if _, err = os.Stat(filepath.Dir(ex) + "/go-server.crt"); err == nil {
+		fmt.Println("run server: https://*:9091")
+		go http.ListenAndServeTLS(":9091", filepath.Dir(ex)+"/go-server.crt", filepath.Dir(ex)+"/go-server.key", nil)
+	}
 	fmt.Println("run server: localhost:9090")
 	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
